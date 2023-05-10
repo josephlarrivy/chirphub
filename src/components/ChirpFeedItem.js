27ac0f402add19674ae627f1ef35f60e843be76a
josephlarrivy <joseph.larrivy@gmail.com>
2023-05-10 18:00:51 -0500

    fighting with github

--- src/components/ChirpFeedItem.js
+++ src/components/ChirpFeedItem.js
@@ -8,7 +8,6 @@ const ChirpFeedItem = ({chirp}) => {
     console.log(chirp)
   }, [])
   
-
   function formatTimestamp(timestamp) {
     const date = new Date(timestamp);
     return date.toLocaleString();
